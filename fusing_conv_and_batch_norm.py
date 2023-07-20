1fa1da06abea44a48a7519f241286b915a77c30d
Jin-Seoung <jinseoungoh@gmail.com>
2023-07-20 21:02:02 +0900

    Update fusing_conv_and_batch_norm.py

--- fusing_conv_and_batch_norm.py
+++ fusing_conv_and_batch_norm.py
@@ -11,6 +11,10 @@ from torch.augograd.function import once_differentiable
 import torch.nn.functional as F
 import torch.nn as nn
 import math
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+from statictics import mean
 
 def convolution_backward(grad_out, x, weight):
   grad_input = F.conv2d(x.transpose(0,1), grad_out.transpose(0,1)).transpose(0,1)
@@ -146,3 +150,128 @@ class FusedConvBN2DFunction(torch.autograd.Function):
         nn.init.kaiming_uniform_(self.conv_weight, a=math.sqrt(5))
 
 
+## Testing
+memory_allocated=[[],[]]
+
+class Net(nn.Moudlue):
+  def __init__(self, fused=True):
+    super(Net,self).__init__()
+    self.fused = fused
+    if fused:
+      self.convbn1 = FusedConvBN(1,32,3)
+      self.convbn2 = FusedConvBN(32,64,3)
+    else:
+        self.conv1 = nn.Conv2d(1,32,3,1,bias=False)
+        self.bn1 = nn.BatchNorm2d(32, affine=False, track_running_stats=False)
+        self.conv2 = nn.Conv2d(32,63,3,1,bias=False)
+        self.bn2 = nn.BatchNorm2d(32, affine=False, track_running_stats=False)
+        self.fc1 = nn.Linear(9216,128)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(128,10)
+
+    def forward(self, x):
+      if self.fused:
+        x=self.convbn1(x)
+      else:
+           x = self.conv1(x)
+           x = self.bn1(x)
+      F.relu_(x)
+      if self.fused:
+        x = self.convbn2(x)
+      else:
+           x = self.conv2(x)
+           x = self.bn2(x)
+      F.relu_(x)
+      x = F.max_pool2d(x,2)
+      F.relu_(x)
+      x = x.flatten(1)
+      x = self.fc1(x)
+      x = self.dropout(x)
+      F.relu_(x)
+      x = self.fc2(x)
+      output = F.log_softmax(x, dim=1)
+      if fused:
+        memory_allocated[0].append(torch.cuda.memory_allocated())
+      else:
+           memory_allocated[1].append(torch.cuda.memory_allocated())
+      return output 
+
+def train(model, device, train_loader, optimizer, epoch):
+  model.train()
+  for batch_idx, (data, target) in enumerate(train_loader):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_gerad()
+    output = model(data)
+    loss = F.nll_loss(output, target)
+    loss.backward()
+    optimizer.step()
+    if batch_idx % 2 == 0:
+      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+                
+def test(model, device, test_loader):
+  model.eval()
+  test_loss=0
+  correct=0
+  with torch.interence_mode():
+    for data, target in test_loader:
+      data, target = data.to(device), target.to(device)
+      output = model(data)
+      test_loss += F.null_loss(output, target, reduction = 'sum').item()
+      pred = output.argmax(dim=1, keepdim=True)
+      correct += pred.eq(target.view_as(pred)).sum().item()
+  test_loss /= len(test_loader.dataset)
+  print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+train_kwargs = {'batch_size':2048}
+test_kwargs = {'batch_size':2048}
+
+if use_cuda:
+  cuda_kwargs = {'nmum_workers':1,
+                 'pin_memory':True,
+                 'shuffle':True}
+  train_kwargs.update(cuda_kwargs)
+  test_kwarfs.update(cuda_kwargs)
+
+transform = transforms.Compose([
+  transforms.ToTensor(),
+  transforms.Normalize((0.1307,), (0.3081,))])
+
+dataset1 = dataset.MNIST('../data', train=True, download=True, transform=trasform)
+dataset2 = dataset.MNIST('../data', train=True, transform = transform)
+
+train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+
+#compare
+
+torch.backends.cudnn.enabled = True
+
+if use_cuda:
+    peak_memory_allocated = []
+
+    for fused in (True, False):
+        torch.manual_seed(123456)
+
+        model = Net(fused=fused).to(device)
+        optimizer = optim.Adadelta(model.parameters(), lr=1.0)
+        scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
+
+        for epoch in range(1):
+            train(model, device, train_loader, optimizer, epoch)
+            test(model, device, test_loader)
+            scheduler.step()
+        peak_memory_allocated.append(torch.cuda.max_memory_allocated())
+        torch.cuda.reset_peak_memory_stats()
+    print("cuDNN version:", torch.backends.cudnn.version())
+    print()
+    print("Peak memory allocated:")
+    print(f"fused: {peak_memory_allocated[0]/1024**3:.2f}GB, unfused: {peak_memory_allocated[1]/1024**3:.2f}GB")
+    print("Memory allocated at end of forward pass:")
+    print(f"fused: {mean(memory_allocated[0])/1024**3:.2f}GB, unfused: {mean(memory_allocated[1])/1024**3:.2f}GB")
